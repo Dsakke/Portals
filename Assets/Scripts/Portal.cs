7d50704c67c79faf33b6d7314891fc9ee7c5ec80
sam vanden bosch <sammeke119@live.com>
2022-03-18 14:28:12 +0100

    initial commit

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_PlayerCamera = null;
+    [SerializeField]
+    private Transform m_OtherPortal = null;
+    
+    private Camera m_PortalCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
